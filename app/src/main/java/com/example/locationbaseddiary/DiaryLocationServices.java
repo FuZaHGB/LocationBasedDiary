8f9c62687424b2d128fb2fc6a1564c19cbb33f57
Daniel Scott <ds86@hw.ac.uk>
2021-01-28 02:00:24 +0000

    Able to query PostgREST API and get appropriate results back... after 5 hours of frantically trying to make it work lol

--- app/src/main/java/com/example/locationbaseddiary/DiaryLocationServices.java
+++ app/src/main/java/com/example/locationbaseddiary/DiaryLocationServices.java
@@ -9,15 +9,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Build;
 import android.os.IBinder;
 import android.os.Looper;
-import android.provider.Settings;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
@@ -26,6 +24,27 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.osgeo.proj4j.BasicCoordinateTransform;
+import org.osgeo.proj4j.CRSFactory;
+import org.osgeo.proj4j.CoordinateReferenceSystem;
+import org.osgeo.proj4j.ProjCoordinate;
+
+import java.io.UnsupportedEncodingException;
+
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.client.HttpClient;
+import cz.msebera.android.httpclient.client.methods.HttpPost;
+import cz.msebera.android.httpclient.entity.StringEntity;
+import cz.msebera.android.httpclient.impl.client.DefaultHttpClient;
+import cz.msebera.android.httpclient.params.BasicHttpParams;
 
 public class DiaryLocationServices extends Service {
 
@@ -34,8 +53,19 @@ public class DiaryLocationServices extends Service {
     static int DEFAULT_LOCATION_UPDATE_INTERVAL = 15000;
     static int FASTEST_LOCATION_UPDATE_INTERVAL = 10000;
 
+    //private static String BASE_URL = "http://188.166.145.15:3000/edinburghpoi?street_name=eq.Hermiston+Gait";
+    private static String BASE_URL = "http://188.166.145.15:3000/rpc/getclosest";
+
+
     private double deviceLat = 0.0;
     private double deviceLong = 0.0;
+
+    CRSFactory factory = new CRSFactory();
+    CoordinateReferenceSystem srcCrs = factory.createFromName("EPSG:4326");
+    CoordinateReferenceSystem dstCrs = factory.createFromName("EPSG:27700");
+
+    BasicCoordinateTransform transform = new BasicCoordinateTransform(srcCrs, dstCrs);
+
 
     FusedLocationProviderClient fusedLocationProviderClient;
     LocationRequest locationRequest;
@@ -49,9 +79,65 @@ public class DiaryLocationServices extends Service {
                 Log.d(TAG, "onLocationResult: " + location.toString());
                 deviceLat = locationResult.getLastLocation().getLatitude();
                 deviceLong = locationResult.getLastLocation().getLongitude();
+
+                try {
+                    jsonQueryPostgREST();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
+            //convertWGStoBNG(deviceLat, deviceLong);
         }
     };
+
+
+    private void jsonQueryPostgREST() throws JSONException, UnsupportedEncodingException {
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.addHeader("Prefer", "params=single-object");
+        client.addHeader("Accept", "application/json");
+        client.addHeader("Content-type", "application/json;charset=utf-8");
+
+        JSONObject json = new JSONObject();
+
+        // Hardcoded for Debug testing.
+        json.put("northing", "671009.583082142");
+        json.put("easting", "321016.697423582");
+        json.put("poiclassname", "Bakeries");
+
+        StringEntity se = new StringEntity(json.toString());
+
+        client.post(this, BASE_URL, se, "application/json;charset=utf-8", new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers,
+                                  JSONArray response) {
+                Log.d(TAG, "JSON: " + response.toString());
+
+                try {
+
+
+                } catch (Exception e) {
+
+                    Log.e(TAG, e.toString());
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable e,
+                                  JSONArray response) {
+
+                // for when the HTTP response is bad (e.g. 400)
+                Log.d(TAG, "Request fail! Status code: " + statusCode);
+                Log.d(TAG, "Fail response: " + response);
+                Log.e(TAG, e.toString());
+
+                Toast.makeText(DiaryLocationServices.this, "Request Failed",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 
     public void beginLocationUpdates(){
         String channelId = "location_notification_channel";
@@ -104,16 +190,6 @@ public class DiaryLocationServices extends Service {
         fusedLocationProviderClient.removeLocationUpdates(locationCallback);
         stopForeground(true);
         stopSelf();
-    }
-
-    public Boolean checkLocationPermissionGranted(){
-        // Cannot ask for Permissions in a service, so this will return a Boolean to activity that calls it.
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                return true;
-            }
-        }
-        return false;
     }
 
     @Override
